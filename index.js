075c827f86a5f2432010d8b0e03ed5647c57b976
jaggerharness <jaggerharness5@gmail.com>
2023-05-11 19:41:54 -0500

    cors dep

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
 morgan.token('req-data', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json());
+app.use(cors())
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -90,7 +93,7 @@ app.get("/info", (req, res) => {
     <p>${Date()}</p></div>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

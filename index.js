0726b68cc05465fec025e27f620551ae5b052dba
jaggerharness <jaggerharness5@gmail.com>
2023-05-15 20:02:12 -0500

    Exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,60 +1,50 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
+const Person = require("./models/persons");
 
-morgan.token('req-data', function (req, res) { return JSON.stringify(req.body) })
+morgan.token("req-data", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 app.use(express.json());
-app.use(express.static('dist'))
-app.use(cors())
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens['req-data'](req, res)
-  ].join(' ')
-}));
+app.use(express.static("dist"));
+app.use(cors());
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["req-data"](req, res),
+    ].join(" ");
+  })
+);
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((personsDB) => {
+    persons = personsDB;
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    return res.status(404).end();
-  }
-  res.json(person);
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((err) => {
+      return res.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -65,28 +55,32 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 app.post("/api/persons", (req, res) => {
-  const id = Math.floor(Math.random() * 500000);
-
   if (!req.body.name || !req.body.number) {
     return res.status(400).json({
       error: "person name and number are required",
     });
   }
 
-  const newPersonData = req.body;
-  const newPerson = { id, ...newPersonData };
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number,
+  });
 
-  if (
-    persons.find(
-      (person) => person.name.toLowerCase() === newPerson.name.toLowerCase()
-    )
-  ) {
-    return res.status(400).json({
-      error: "person name must be unique",
-    });
-  }
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
+  newPerson.save().then((savedPerson) => {
+    persons = persons.concat(savedPerson);
+    res.json(savedPerson);
+  });
+
+  // Commenting until later
+  // if (
+  //   persons.find(
+  //     (person) => person.name.toLowerCase() === newPerson.name.toLowerCase()
+  //   )
+  // ) {
+  //   return res.status(400).json({
+  //     error: "person name must be unique",
+  //   });
+  // }
 });
 
 app.get("/info", (req, res) => {
